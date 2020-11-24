00e51edc063f599799efa258af9c9e25dd7cf44c
microshine <microshine@mail.ru>
2020-11-24 16:11:38 +0300

    feat: optimize and improve X500 name

--- test/crypto.ts
+++ test/crypto.ts
@@ -115,6 +115,13 @@ context("crypto", () => {
       assert.strictEqual(Convert.ToHex(name.toArrayBuffer()), "3071310e300c060355040313056e616d65313139300c060355040313056e616d6532300c060355040313056e616d6533301b06092a864886f70d010901160e736f6d6540656d61696c2e636f6d3124300a06042a030405040201023016060a0992268993f22c6401191608736f6d652e636f6d");
     });
 
+    it("parse with odd , marks", () => {
+      const text = "  ,  , ,  CN=Some Name, O=Peculiar Ventures\\, LLC, O=\"Peculiar Ventures, LLC\", CN=name2+O=Test+CN=name3+E=some@email.com, 1.2.3.4.5=#04020102+DC=some.com,, , ";
+      const name = new x509.Name(text);
+
+      assert.strictEqual(name.toString(), "CN=Some Name, O=Peculiar Ventures\\, LLC, O=Peculiar Ventures\\, LLC, CN=name2+O=Test+CN=name3+E=some@email.com, 1.2.3.4.5=#04020102+DC=some.com");
+    });
+
   });
 
   context("Pkcs10CertificateRequest", () => {
