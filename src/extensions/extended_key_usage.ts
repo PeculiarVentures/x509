bc097b1915300507797c59d941d623e5db290267
microshine <microshine@mail.ru>
2022-11-09 16:44:13 +0300

    feat: add ExtendedKeyUsage enum - fixes #37

--- src/extensions/extended_key_usage.ts
+++ src/extensions/extended_key_usage.ts
@@ -1,7 +1,18 @@
-import { ExtendedKeyUsage, id_ce_extKeyUsage } from "@peculiar/asn1-x509";
+import * as asn1X509 from "@peculiar/asn1-x509";
 import { AsnConvert } from "@peculiar/asn1-schema";
 import { BufferSourceConverter } from "pvtsutils";
 import { Extension } from "../extension";
+
+export enum ExtendedKeyUsage {
+  serverAuth = "1.3.6.1.5.5.7.3.1",
+  clientAuth = "1.3.6.1.5.5.7.3.2",
+  codeSigning = "1.3.6.1.5.5.7.3.3",
+  emailProtection = "1.3.6.1.5.5.7.3.4",
+  timeStamping = "1.3.6.1.5.5.7.3.8",
+  ocspSigning = "1.3.6.1.5.5.7.3.9",
+}
+
+export type ExtendedKeyUsageType = asn1X509.ExtendedKeyUsage | string;
 
 /**
  * Represents the Extended Key Usage certificate extension
@@ -11,7 +22,7 @@ export class ExtendedKeyUsageExtension extends Extension {
   /**
    * Gets a list of purposes for which the certified public key may be used
    */
-  public readonly usages: string[];
+  public readonly usages: ExtendedKeyUsageType[];
 
   /**
    * Creates a new instance from DER encoded buffer
@@ -23,16 +34,16 @@ export class ExtendedKeyUsageExtension extends Extension {
    * @param usages
    * @param critical
    */
-  public constructor(usages: string[], critical?: boolean);
+  public constructor(usages: ExtendedKeyUsageType[], critical?: boolean);
   public constructor(...args: any[]) {
     if (BufferSourceConverter.isBufferSource(args[0])) {
       super(args[0]);
 
-      const value = AsnConvert.parse(this.value, ExtendedKeyUsage);
+      const value = AsnConvert.parse(this.value, asn1X509.ExtendedKeyUsage);
       this.usages = value.map(o => o);
     } else {
-      const value = new ExtendedKeyUsage(args[0]);
-      super(id_ce_extKeyUsage, args[1], AsnConvert.serialize(value));
+      const value = new asn1X509.ExtendedKeyUsage(args[0]);
+      super(asn1X509.id_ce_extKeyUsage, args[1], AsnConvert.serialize(value));
 
       this.usages = args[0];
     }
