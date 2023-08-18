744b365e4df706cb50a887b018936cdf0361f3bb
microshine <microshine@mail.ru>
2023-08-18 13:05:19 +0200

    chore: use @rollup/plugin-terser

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,7 +6,7 @@ import { getBabelOutputPlugin } from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import dts from "rollup-plugin-dts";
 import typescript from "rollup-plugin-typescript2";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 import pkg from "./package.json" assert { type: "json" };
 
 const __filename = url.fileURLToPath(import.meta.url);
